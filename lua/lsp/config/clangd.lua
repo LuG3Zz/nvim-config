c16d8dcb38d559b9ca78de9e9eea05bcdbdf2b5f
影翼 <41784264+jinzhongjia@users.noreply.github.com>
2022-10-03 23:00:43 +0800

    更换被丢弃的函数

--- lua/lsp/config/clangd.lua
+++ lua/lsp/config/clangd.lua
@@ -1,8 +1,12 @@
 local opt = {
 	settings = {},
+	cmd = { "clangd", "--offset-encoding=utf-16" },
 	flags = {
 		debounce_text_changes = 150,
 	},
+	root_dir = function()
+		return vim.fn.getcwd()
+	end,
 	on_attach = function(client, bufnr)
 		-- 禁用格式化功能，交给专门插件插件处理
 		client.server_capabilities.documentFormattingProvider = false
@@ -14,7 +18,7 @@ local opt = {
 		-- 绑定快捷键
 		require("keybindings").mapLSP(buf_set_keymap)
 		-- 保存时自动格式化
-		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format()")
 	end,
 }
 
