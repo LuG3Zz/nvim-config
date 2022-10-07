a17da5b1c1384487d19f9e57c304b220eb9bd2c6
jinzhongjia <jinzhongjia@qq.com>
2022-10-08 00:06:46 +0800

    coc的基本配置

--- init.lua
+++ init.lua
@@ -6,3 +6,21 @@ require("keybindings")
 
 -- Packer 插件管理
 require("plugins")
+
+-- 主题设置
+require("colorscheme")
+
+-- 侧边文件浏览器
+require("plugin-config.nvim-tree")
+
+-- bufferline
+require("plugin-config.bufferline")
+
+-- lualine 
+require("plugin-config.lualine")
+
+-- telescope
+require("plugin-config.telescope")
+
+-- coc
+require("plugin-config.coc")
---
+++ lua/plugin-config/lualine.lua
@@ -0,0 +1,34 @@
+-- 如果找不到lualine 组件，就不继续执行
+local status, lualine = pcall(require, "lualine")
+if not status then
+    vim.notify("没有找到 lualine")
+  return
+end
+
+lualine.setup({
+  options = {
+    theme = "tokyonight",
+    component_separators = { left = "|", right = "|" },
+    -- https://github.com/ryanoasis/powerline-extra-symbols
+    section_separators = { left = " ", right = "" },
+  },
+  extensions = { "nvim-tree", "toggleterm" },
+  sections = {
+    lualine_c = {
+      "filename",
+    },
+    lualine_x = {
+      "filesize",
+      {
+        "fileformat",
+         symbols = {
+           unix = '', -- e712
+           dos = '', -- e70f
+           mac = '', -- e711
+         },
+      },
+      "encoding",
+      "filetype",
+    },
+  },
+})
