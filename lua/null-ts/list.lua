1cd72483da4fe811f4c0794391ab8ad46899abbe
jinzhongjia <jinzhongjia@qq.com>
2022-10-17 14:25:27 +0800

    null-ts configuration extraction

---
+++ lua/null-ts/list.lua
@@ -0,0 +1,23 @@
+local code_action = "code_actions"
+local completion = "completion"
+local diagnostics = "diagnostics"
+local formatting = "formatting"
+local hover = "hover"
+
+local list = {
+	{ "clang_format", { formatting } },
+	{ "cmake_format", { formatting } },
+	{ "gofumpt", { formatting } },
+	{ "goimports", { formatting } },
+	{ "golines", { formatting } },
+	{ "staticcheck", { diagnostics } },
+	{ "eslint_d", { code_action } },
+	{ "markdownlint", { formatting, diagnostics } },
+	{ "prettier", { formatting } },
+	{ "shellcheck", { code_action } },
+	{ "stylua", { formatting } },
+	{ "jq", { formatting } },
+	{ "shfmt", { formatting } },
+}
+
+return list
--- lua/null-ts/setup.lua
+++ lua/null-ts/setup.lua
@@ -1,71 +1,91 @@
 local mason_null_ls = require("mason-null-ls")
 local null_ls = require("null-ls")
 
+local list = require("null-ts.list")
+local sources = {}
+local handlers = {}
+
+for _, ele in pairs(list) do
+	local name = ele[1]
+	local attrs = ele[2]
+	-- insert name to sources
+	table.insert(sources, name)
+	-- create func of name to register
+	handlers[name] = function()
+		for _, attr in pairs(attrs) do
+			null_ls.register(null_ls.builtins[attr][name])
+		end
+	end
+end
+
 mason_null_ls.setup({
-	ensure_installed = {
-		"clang_format",
-		"cmake_format",
-		"gofumpt",
-		"goimports",
-		"golines",
-		"staticcheck",
-		"eslint_d",
-		"markdownlint",
-		"prettier",
-		"shellcheck",
-		"stylua",
-		"jq",
-		"shfmt",
-		-- "cspell",
-	},
+	ensure_installed = sources,
+	--    {
+	-- 	"clang_format",
+	-- 	"cmake_format",
+	-- 	"gofumpt",
+	-- 	"goimports",
+	-- 	"golines",
+	-- 	"staticcheck",
+	-- 	"eslint_d",
+	-- 	"markdownlint",
+	-- 	"prettier",
+	-- 	"shellcheck",
+	-- 	"stylua",
+	-- 	"jq",
+	-- 	"shfmt",
+	-- 	-- "cspell",
+	-- },
 })
 
-mason_null_ls.setup_handlers({
-	clang_format = function()
-		null_ls.register(null_ls.builtins.formatting.clang_format)
-	end,
-	cmake_format = function()
-		null_ls.register(null_ls.builtins.formatting.cmake_format)
-	end,
-	stylua = function()
-		null_ls.register(null_ls.builtins.formatting.stylua)
-	end,
-	prettier = function()
-		null_ls.register(null_ls.builtins.formatting.prettier)
-	end,
-	gofumpt = function()
-		null_ls.register(null_ls.builtins.formatting.gofumpt)
-	end,
-	goimports = function()
-		null_ls.register(null_ls.builtins.formatting.goimports)
-	end,
-	golines = function()
-		null_ls.register(null_ls.builtins.formatting.golines)
-	end,
-	jq = function()
-		null_ls.register(null_ls.builtins.formatting.jq)
-	end,
-	staticcheck = function()
-		null_ls.register(null_ls.builtins.diagnostics.staticcheck)
-	end,
-	eslint_d = function()
-		null_ls.register(null_ls.builtins.code_actions.eslint_d)
-	end,
-	shellcheck = function()
-		null_ls.register(null_ls.builtins.code_actions.shellcheck)
-	end,
-	markdownlint = function()
-		null_ls.register(null_ls.builtins.diagnostics.markdownlint)
-		null_ls.register(null_ls.builtins.formatting.markdownlint)
-	end,
-	shfmt = function()
-		null_ls.register(null_ls.builtins.formatting.shfmt)
-	end,
-	-- cspell = function()
-	-- 	null_ls.register(null_ls.builtins.diagnostics.cspell)
-	-- 	null_ls.register(null_ls.builtins.code_actions.cspell)
-	-- end,
-})
+mason_null_ls.setup_handlers(handlers)
+
+-- mason_null_ls.setup_handlers({
+-- 	clang_format = function()
+-- 		null_ls.register(null_ls.builtins.formatting.clang_format)
+-- 	end,
+-- 	cmake_format = function()
+-- 		null_ls.register(null_ls.builtins.formatting.cmake_format)
+-- 	end,
+-- 	stylua = function()
+-- 		null_ls.register(null_ls.builtins.formatting.stylua)
+-- 	end,
+-- 	prettier = function()
+-- 		null_ls.register(null_ls.builtins.formatting.prettier)
+-- 	end,
+-- 	gofumpt = function()
+-- 		null_ls.register(null_ls.builtins.formatting.gofumpt)
+-- 	end,
+-- 	goimports = function()
+-- 		null_ls.register(null_ls.builtins.formatting.goimports)
+-- 	end,
+-- 	golines = function()
+-- 		null_ls.register(null_ls.builtins.formatting.golines)
+-- 	end,
+-- 	jq = function()
+-- 		null_ls.register(null_ls.builtins.formatting.jq)
+-- 	end,
+-- 	staticcheck = function()
+-- 		null_ls.register(null_ls.builtins.diagnostics.staticcheck)
+-- 	end,
+-- 	eslint_d = function()
+-- 		null_ls.register(null_ls.builtins.code_actions.eslint_d)
+-- 	end,
+-- 	shellcheck = function()
+-- 		null_ls.register(null_ls.builtins.code_actions.shellcheck)
+-- 	end,
+-- 	markdownlint = function()
+-- 		null_ls.register(null_ls.builtins.diagnostics.markdownlint)
+-- 		null_ls.register(null_ls.builtins.formatting.markdownlint)
+-- 	end,
+-- 	shfmt = function()
+-- 		null_ls.register(null_ls.builtins.formatting.shfmt)
+-- 	end,
+-- 	-- cspell = function()
+-- 	-- 	null_ls.register(null_ls.builtins.diagnostics.cspell)
+-- 	-- 	null_ls.register(null_ls.builtins.code_actions.cspell)
+-- 	-- end,
+-- })
 
 null_ls.setup({
 	debug = false,
