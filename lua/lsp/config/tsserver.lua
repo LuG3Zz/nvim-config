d062613f4069c11143515978943f26c011a6bcec
jinzhongjia <jinzhongjia@qq.com>
2022-10-18 23:16:23 +0800

    Modify lsp logic to simplify file structure

--- lua/lsp/config/tsserver.lua
+++
@@ -1,3 +0,0 @@
-local opt = {}
-
-return opt
--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -4,30 +4,26 @@ local lspconfig = require("lspconfig")
 
 local servers_handlers = {}
 
-local default_config=require("lsp.default-config")
+local default_config = require("lsp.default-config")
 
 for _, value in pairs(servers) do
-  local status, config = pcall(require, "lsp.config." .. value)
-  if not status then
-    vim.notify("lsp configuration not found" .. value)
-    goto continue
-  end
-  servers_handlers[value]=function()
-    lspconfig[value].setup(vim.tbl_deep_extend("force",default_config,config))
-  end
-  ::continue::
+	local status, config = pcall(require, "lsp.config." .. value)
+	if not status then
+		config = {}
+	end
+	servers_handlers[value] = function()
+		lspconfig[value].setup(vim.tbl_deep_extend("force", default_config, config))
+	end
 end
 
 mason_lspconfig.setup({
-  ensure_installed = servers
+	ensure_installed = servers,
 })
 
 mason_lspconfig.setup_handlers(servers_handlers)
-
 
 -- Code completion
 require("lsp.cmp")
 
 -- ui completion
 require("lsp.ui")
-
