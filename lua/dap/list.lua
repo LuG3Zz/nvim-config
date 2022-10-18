19cfdcfbe8337b6926087e6ee9ea8f2fec46346c
jinzhongjia <jinzhongjia@qq.com>
2022-10-18 23:50:42 +0800

    Separate dap file introduction to simplify logic

---
+++ lua/dap/list.lua
@@ -0,0 +1,20 @@
+local list = {
+	{
+		name = "python",
+		part = true,
+	},
+	{
+		name = "delve",
+		part = true,
+	},
+	{
+		name = "cppdbg",
+		part = true,
+	},
+	{
+		name = "js",
+		part = true,
+	},
+}
+
+return list
--- lua/dap/setup.lua
+++ lua/dap/setup.lua
@@ -6,25 +6,36 @@ end
 -- import dap's config
 require("dap.dap-config")
 
+local list = require("dap.list")
+
+local handlers = {}
+local parts = {}
+local sources = {}
+
+for _, ele in pairs(list) do
+	table.insert(sources, ele.name)
+	if ele.part then
+		table.insert(parts, "dap.config." .. ele.name)
+	else
+		handlers[ele.name] = require("dap.config." .. ele.name)
+	end
+end
+
 -- list the debug dependence that must be installed
 mason_dap.setup({
-	ensure_installed = {
-		"python",
-		"delve",
-		"node2",
-		"cppdbg",
-		"js",
-	},
+	ensure_installed = sources,
 })
 
 -- config dap
-mason_dap.setup_handlers({
-	cppdbg = require("dap.config.cpptools"),
-})
+mason_dap.setup_handlers(handlers)
 
--- debug depend nvim-dap-go
-require("dap.config.go")
--- debug depend nvim-dap-python
-require("dap.config.python")
--- debug depend nvim-dap-vscode-js
-require("dap.config.js")
+for _, ele in pairs(parts) do
+	require(ele)
+end
+
+-- -- debug depend nvim-dap-go
+-- require("dap.config.go")
+-- -- debug depend nvim-dap-python
+-- require("dap.config.python")
+-- -- debug depend nvim-dap-vscode-js
+-- require("dap.config.js")
