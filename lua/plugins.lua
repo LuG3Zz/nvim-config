2db992879bdd32b63b41a0f90704c24f6036b015
jinzhongjia <jinzhongjia@qq.com>
2022-10-30 05:49:17 +0800

    add packer Bootstrapping

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 local packer = require("packer")
 packer.startup({
 	function(use)
@@ -132,6 +145,9 @@ packer.startup({
 		use("projekt0n/github-nvim-theme")
 		use("EdenEast/nightfox.nvim")
 		-------------------------------------------------------
+		if packer_bootstrap then
+			require("packer").sync()
+		end
 	end,
 	config = {
 		-- Concurrency limit
