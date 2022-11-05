55228c22574541ba1af55e68f621fed4087ee43a
jinzhongjia <jinzhongjia@qq.com>
2022-11-06 02:07:30 +0800

    replace python's lsp pyright to jedi_language_server

--- lua/lsp/list.lua
+++ lua/lsp/list.lua
@@ -14,7 +14,9 @@ local list = {
 	{ name = "tsserver", alone = false },
 	-- { name = "psalm", alone = false },
 	-- { name = "powershell_es", alone = false },
-	{ name = "pyright", alone = false },
+	-- { name = "pyright", alone = false },
+	{ name = "jedi_language_server", alone = false },
+	-- { name = "pylsp", alone = false },
 	{ name = "sqls", alone = false }, -- need extra yaml configuration
 	{ name = "tailwindcss", alone = true },
 	{ name = "volar", alone = false }, -- Can be customized to override the default tsserver
